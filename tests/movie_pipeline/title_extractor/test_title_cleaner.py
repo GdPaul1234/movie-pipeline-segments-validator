57b6b676b3a2428af114c44f1ed52e610a68f9ac
GdPaul1234 <paul.godin1234@outlook.fr>
2023-10-24 23:55:53 +0200

    chore: import tests

---
+++ tests/movie_pipeline/title_extractor/test_title_cleaner.py
@@ -0,0 +1,20 @@
+from pathlib import Path
+import unittest
+
+from movie_pipeline.lib.title_extractor.title_cleaner import TitleCleaner
+
+blacklist_path = Path(__file__).parent.parent.joinpath('ressources', 'test_title_re_blacklist.txt')
+
+
+class TestTitleCleaner(unittest.TestCase):
+    def test_clean_title_remove_junk_following_blacklist(self):
+        title_cleaner = TitleCleaner(blacklist_path)
+        actual_title = title_cleaner.clean_title('Arrête-moi si tu peux (VM)')
+
+        self.assertEqual('Arrête-moi si tu peux', actual_title)
+
+    def test_clean_title_remove_episode_number_following_blacklist(self):
+        title_cleaner = TitleCleaner(blacklist_path)
+        actual =  title_cleaner.clean_title("Le peuple des forêts (1-3) - L'âge de glace")
+
+        self.assertEqual("Le peuple des forêts - L'âge de glace", actual)
---
+++ tests/movie_pipeline/title_extractor/test_title_extractor.py
@@ -0,0 +1,124 @@
+from contextlib import contextmanager
+from pathlib import Path
+import json
+import unittest
+
+from movie_pipeline.lib.title_extractor.title_cleaner import TitleCleaner
+from movie_pipeline.lib.title_extractor.title_extractor import (
+    NaiveTitleExtractor,
+    SerieSubTitleAwareTitleExtractor,
+    SerieTitleAwareTitleExtractor,
+    SubtitleTitleExpanderExtractor
+)
+from movie_pipeline.services.edl_scaffolder import MovieProcessedFileGenerator
+
+movie_metadata_path = Path(__file__).parent.joinpath('Channel 1_Movie Name_2022-12-05-2203-20.ts.metadata.json')
+serie_metadata_path = Path(__file__).parent.joinpath("Channel 1_Serie Name. 'Title..._2022-12-05-2203-20.ts.metadata.json")
+
+blacklist_path = Path(__file__).parent.parent.joinpath('ressources', 'test_title_re_blacklist.txt')
+default_title_cleaner = TitleCleaner(blacklist_path)
+
+
+@contextmanager
+def file_path_with_metadata_content(content: str, metadata_path: Path):
+    metadata_path.write_text(content)
+    file_path = metadata_path.with_name(metadata_path.name.removesuffix('.metadata.json'))
+    file_path.touch()
+
+    try:
+        yield file_path
+    finally:
+        metadata_path.unlink()
+        file_path.unlink()
+
+
+class TestTitleExtractor(unittest.TestCase):
+    def test_movie_naive_title_extractor(self):
+        movie_file_path = movie_metadata_path.with_name(movie_metadata_path.name.removesuffix('.metadata.json'))
+        title_extractor = NaiveTitleExtractor(default_title_cleaner)
+        extracted_title = title_extractor.extract_title(movie_file_path)
+
+        self.assertEqual('Movie Name', extracted_title)
+
+    def test_serie_naive_title_extractor(self):
+        serie_file_path = serie_metadata_path.with_name(serie_metadata_path.name.removesuffix('.metadata.json'))
+        title_extractor = NaiveTitleExtractor(default_title_cleaner)
+        extracted_title = title_extractor.extract_title(serie_file_path)
+
+        self.assertEqual("Serie Name. 'Title...", extracted_title)
+
+    def test_movie_subtitle_title_expander_title_extractor(self):
+        content = json.dumps({
+            "title": "Movie Name...",
+            "sub_title": "Movie Name... : Movie Name, le titre long. Bla Bla Bla"
+        }, indent=2)
+
+        with file_path_with_metadata_content(content, movie_metadata_path) as movie_file_path:
+            title_extractor = SubtitleTitleExpanderExtractor(default_title_cleaner)
+            extracted_title = title_extractor.extract_title(movie_file_path)
+            self.assertEqual("Movie Name, le titre long", extracted_title)
+
+    def test_serie_subtitle_title_expander_title_extractor(self):
+        content = json.dumps({
+            "title": "Serie Name. \"Title...",
+            "sub_title": "Serie Name. 'Title overflow!' Série (FR)"
+        }, indent=2)
+
+        with file_path_with_metadata_content(content, serie_metadata_path) as serie_file_path:
+            title_extractor = SubtitleTitleExpanderExtractor(default_title_cleaner)
+            extracted_title = title_extractor.extract_title(serie_file_path)
+            self.assertEqual("Serie Name__Title overflow!", extracted_title)
+
+    def test_serie_subtitle_aware_title_extractor(self):
+        test_serie_metadata_path = serie_metadata_path.with_name('Channel 1_Serie Name_2022-12-05-2203-20.ts.metadata.json')
+        content = json.dumps({
+            "title": "Serie Name",
+            "sub_title": "Serie Name : Episode Name. Série policière. 2022. Saison 1. 16/26.",
+            "description": ""
+        }, indent=2)
+
+        with file_path_with_metadata_content(content, test_serie_metadata_path) as serie_file_path:
+            title_extractor = SerieSubTitleAwareTitleExtractor(default_title_cleaner)
+            extracted_title = title_extractor.extract_title(serie_file_path)
+            self.assertEqual("Serie Name S01E16", extracted_title)
+
+    def test_serie_title_aware_title_extractor(self):
+        test_serie_metadata_path = serie_metadata_path.with_name('Channel 1_Serie Name_2022-12-05-2203-20.ts.metadata.json')
+        content = json.dumps({
+            "title": "Serie Name (2-3)",
+            "sub_title": "",
+            "description": "Série documentaire (France, 2022, 52 min) Description",
+        }, indent=2)
+
+        with file_path_with_metadata_content(content, test_serie_metadata_path) as serie_file_path:
+            title_extractor = SerieTitleAwareTitleExtractor(default_title_cleaner)
+            extracted_title = title_extractor.extract_title(serie_file_path)
+            self.assertEqual("Serie Name S01E02", extracted_title)
+
+    def test_serie_title_aware_title_with_season_extractor(self):
+        test_serie_metadata_path = serie_metadata_path.with_name('Channel 1_Serie Name_2022-12-05-2203-20.ts.metadata.json')
+        content = json.dumps({
+            "title": "Serie Name - Saison 2 (4-6) (VM)",
+            "sub_title": "",
+            "description": "Série documentaire (France, 2022, 52 min) Description",
+        }, indent=2)
+
+        with file_path_with_metadata_content(content, test_serie_metadata_path) as serie_file_path:
+            title_extractor = SerieTitleAwareTitleExtractor(default_title_cleaner)
+            extracted_title = title_extractor.extract_title(serie_file_path)
+            self.assertEqual("Serie Name S02E04", extracted_title)
+
+    def test_extract_serie_title_from_series_extracted_metadata(self):
+        serie_file_path = Path(__file__).parent.joinpath("Channel 1_Serie Name 'Episode Name'_2022-12-05-2203-20.ts")
+        default_title_extractor = NaiveTitleExtractor(default_title_cleaner)
+
+        series_extracted_metadata = {
+            'Serie Name': {
+                'Episode Name': {
+                    'formattedEpisode': 'S03E42'
+                }
+            }
+        }
+
+        edl_template = MovieProcessedFileGenerator(serie_file_path, default_title_extractor, series_extracted_metadata)
+        self.assertEqual('Serie Name S03E42', edl_template.extract_title())
