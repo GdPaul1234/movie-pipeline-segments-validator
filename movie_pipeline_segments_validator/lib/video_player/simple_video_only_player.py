30ba7ae2ff8296dbff91132f93d75ebddf2b681b
GdPaul1234 <paul.godin1234@outlook.fr>
2025-04-15 22:47:49 +0200

    feat: get media video frame in API

--- movie_pipeline_segments_validator/lib/video_player/simple_video_only_player.py
+++ movie_pipeline_segments_validator/lib/video_player/simple_video_only_player.py
@@ -11,11 +11,11 @@ from .video_player import IVideoPlayer
 
 logger = logging.getLogger(__name__)
 
-def extract_frame(stream, position_s):
+def extract_frame(stream, position_s, **kwargs):
     out, _ = (
         stream
-        .output('pipe:', format='rawvideo', pix_fmt='rgb24', vframes=1)
-        .run(cmd=['ffmpeg', '-ss', str(position_s)], capture_stdout=True, capture_stderr=True)
+        .output('pipe:', format='rawvideo', pix_fmt='rgb24', vframes=1, **kwargs)
+        .run(cmd=['ffmpeg', '-hide_banner', '-ss', str(position_s)], capture_stdout=True, capture_stderr=True)
     )
 
     return out
