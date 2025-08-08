238f3412518a7c8656c11e361d4779a73998aacf
GdPaul1234 <paul.godin1234@outlook.fr>
2025-08-08 23:13:27 +0200

    chore: add androidx-datastore dependencies, remove wasm target

--- MoviePipelineSegmentsValidatorUi/composeApp/src/wasmJsMain/kotlin/com/gdpaul1234/movie_pipeline_segments_validator_ui/Platform.wasmJs.kt
+++
@@ -1,7 +0,0 @@
-package com.gdpaul1234.movie_pipeline_segments_validator_ui
-
-class WasmPlatform: Platform {
-    override val name: String = "Web with Kotlin/Wasm"
-}
-
-actual fun getPlatform(): Platform = WasmPlatform()
