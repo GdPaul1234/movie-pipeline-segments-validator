241d9aa4b487ac7df92f5643549656176965f846
GdPaul1234 <paul.godin1234@outlook.fr>
2025-08-18 01:17:12 +0200

    feat: WIP usable RecentSessionsScreen

--- MoviePipelineSegmentsValidatorUi/composeApp/src/commonMain/kotlin/com/gdpaul1234/movie_pipeline_segments_validator_ui/App.kt
+++ MoviePipelineSegmentsValidatorUi/composeApp/src/commonMain/kotlin/com/gdpaul1234/movie_pipeline_segments_validator_ui/App.kt
@@ -1,49 +1,19 @@
 package com.gdpaul1234.movie_pipeline_segments_validator_ui
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.safeContentPadding
-import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
-import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import org.jetbrains.compose.resources.painterResource
+import androidx.compose.runtime.Composable
+import com.gdpaul1234.movie_pipeline_segments_validator_ui.core.database.SessionsRepository
+import com.gdpaul1234.movie_pipeline_segments_validator_ui.core.database.rememberDataStore
+import com.gdpaul1234.movie_pipeline_segments_validator_ui.sessions.presentation.ui.RecentSessionsScreen
 import org.jetbrains.compose.ui.tooling.preview.Preview
-
-import moviepipelinesegmentsvalidatorui.composeapp.generated.resources.Res
-import moviepipelinesegmentsvalidatorui.composeapp.generated.resources.compose_multiplatform
 
 @Composable
 @Preview
 fun App() {
+    val dataStore = rememberDataStore()
+    val sessionsRepository = SessionsRepository(dataStore)
+
     MaterialTheme {
-        var showContent by remember { mutableStateOf(false) }
-        Column(
-            modifier = Modifier
-                .background(MaterialTheme.colorScheme.primaryContainer)
-                .safeContentPadding()
-                .fillMaxSize(),
-            horizontalAlignment = Alignment.CenterHorizontally,
-        ) {
-            Button(onClick = { showContent = !showContent }) {
-                Text("Click me!")
-            }
-            AnimatedVisibility(showContent) {
-                val greeting = remember { Greeting().greet() }
-                Column(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                ) {
-                    Image(painterResource(Res.drawable.compose_multiplatform), null)
-                    Text("Compose: $greeting")
-                }
-            }
-        }
+        RecentSessionsScreen(sessionsRepository)
     }
 }
--- MoviePipelineSegmentsValidatorUi/composeApp/src/commonMain/kotlin/com/gdpaul1234/movie_pipeline_segments_validator_ui/sessions/presentation/component/SessionList.kt
+++ MoviePipelineSegmentsValidatorUi/composeApp/src/commonMain/kotlin/com/gdpaul1234/movie_pipeline_segments_validator_ui/sessions/presentation/component/SessionList.kt
@@ -4,19 +4,13 @@ package com.gdpaul1234.movie_pipeline_segments_validator_ui.sessions.presentatio
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.offset
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.Card
-import androidx.compose.material3.ListItem
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import moviepipelinesegmentsvalidatorui.composeapp.generated.resources.*
@@ -32,12 +26,16 @@ import kotlin.time.Duration.Companion.minutes
 import kotlin.time.Duration.Companion.seconds
 import kotlin.time.ExperimentalTime
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 @Preview
 fun SessionList(
     @PreviewParameter(SessionEntriesPreviewParameterProvider::class) sessionEntries: Set<Map.Entry<String, Session>>,
+    @PreviewParameter(CurrentSelectedSessionKeyPreviewParameterProvider::class) currentSelectedSessionKey: String,
     onItemClick: ((Map.Entry<String, Session>) -> Unit)?
 ) {
+    val topAppBarScrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+
     val dummyNewSessionEntry = mapOf(
         "new_session" to Session(
             id = "new_session",
@@ -48,27 +46,42 @@ fun SessionList(
         )
     ).entries.first()
 
-    Card {
-        LazyColumn {
-            item {
-                ListItem(
-                    headlineContent = {
-                        Text(
-                            text = stringResource(Res.string.app_name),
-                            style = MaterialTheme.typography.titleLarge
-                        )
-                    }
-                )
-            }
+    @Composable
+    fun listContentBackgroundColor(sessionEntry: Map.Entry<String, Session>) =
+        when(sessionEntry.key == currentSelectedSessionKey) {
+            true -> MaterialTheme.colorScheme.surfaceVariant
+            else -> MaterialTheme.colorScheme.surface
+        }
 
+
+    Scaffold(
+        topBar = {
+            LargeTopAppBar(
+                scrollBehavior = topAppBarScrollBehavior,
+                title = {
+                    Text(
+                        text = stringResource(Res.string.app_name),
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis
+                    )
+                }
+            )
+        }
+    ) { paddingValues ->
+        LazyColumn(
+            modifier = Modifier
+                .nestedScroll(topAppBarScrollBehavior.nestedScrollConnection)
+                .padding(paddingValues)
+                .consumeWindowInsets(paddingValues)
+        ) {
             item {
                 ListItem(
                     modifier = Modifier
                         .padding(top = 16.dp)
-                        .background(MaterialTheme.colorScheme.primaryContainer)
                         .clickable { onItemClick?.let { it(dummyNewSessionEntry) } },
+                    colors = ListItemDefaults.colors(listContentBackgroundColor(dummyNewSessionEntry)),
                     headlineContent = { Text(stringResource(Res.string.create_session)) },
-                    supportingContent = { Text(stringResource(Res.string.create_session_supporting_text)) }
+                    supportingContent = { Text(stringResource(Res.string.create_session_supporting_text)) },
                 )
             }
 
@@ -94,9 +107,8 @@ fun SessionList(
                     val title = "${id.substring(0..16)}@${endpoint.substringAfter("://")}"
 
                     ListItem(
-                        modifier = Modifier
-                            .background(MaterialTheme.colorScheme.primaryContainer)
-                            .clickable { onItemClick?.let { it(sessionEntry) } },
+                        modifier = Modifier.clickable { onItemClick?.let { it(sessionEntry) } },
+                        colors = ListItemDefaults.colors(listContentBackgroundColor(sessionEntry)),
                         headlineContent = {
                             Row(
                                 horizontalArrangement = Arrangement.SpaceBetween,
@@ -116,7 +128,7 @@ fun SessionList(
     }
 }
 
-class SessionEntriesPreviewParameterProvider : PreviewParameterProvider<Set<Map.Entry<String, Session>>> {
+private class SessionEntriesPreviewParameterProvider : PreviewParameterProvider<Set<Map.Entry<String, Session>>> {
     override val values = sequenceOf(
         setOf(
             Session(
@@ -141,5 +153,14 @@ class SessionEntriesPreviewParameterProvider : PreviewParameterProvider<Set<Map.
                 medias = emptyMap()
             )
         ).associateBy { "${it.id}@http://localhost:8000" }.entries
+    )
+}
+
+
+private class CurrentSelectedSessionKeyPreviewParameterProvider : PreviewParameterProvider<String?> {
+    override val values = sequenceOf(
+        "new_session",
+        "35e69f9ee84f494d9dcc6c62a964b171",
+        ""
     )
 }
