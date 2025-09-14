2e045371f9c9dc36f6732478ed7e85ce43d82f6f
GdPaul1234 <paul.godin1234@outlook.fr>
2025-09-15 00:09:19 +0200

    fix: remove PySimpleGUI adapters

--- movie_pipeline_segments_validator/adapters/pysimplegui/views/texts.py
+++
@@ -1,6 +0,0 @@
-TEXTS = {
-    'movies_to_be_processed': 'Movies to be reviewed',
-    'movie_to_be_validated': 'Movie to be validated',
-    'review_segments_description': 'Review the segments in the right, then click on "Validate"',
-    'loading_media': 'Loading media...'
-}
