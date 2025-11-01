79d31d36a652069f1b7d56183718a0866bb809ae
GdPaul1234 <paul.godin1234@outlook.fr>
2025-11-01 21:56:59 +0100

    chore: replace CircularProgressIndicator with ContainedLoadingIndicator

--- MoviePipelineSegmentsValidatorUi/composeApp/src/commonMain/kotlin/com/gdpaul1234/movie_pipeline_segments_validator_ui/core/presentation/component/LoadingSuspense.kt
+++ MoviePipelineSegmentsValidatorUi/composeApp/src/commonMain/kotlin/com/gdpaul1234/movie_pipeline_segments_validator_ui/core/presentation/component/LoadingSuspense.kt
@@ -3,13 +3,13 @@ package com.gdpaul1234.movie_pipeline_segments_validator_ui.core.presentation.co
 import androidx.compose.animation.Crossfade
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.width
-import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ContainedLoadingIndicator
+import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 
+@OptIn(ExperimentalMaterial3ExpressiveApi::class)
 @Composable
 fun LoadingSuspense(loading: Boolean, content: @Composable () -> Unit) {
     Crossfade(loading) { loading ->
@@ -17,7 +17,7 @@ fun LoadingSuspense(loading: Boolean, content: @Composable () -> Unit) {
             loading -> Box(
                 contentAlignment = Alignment.Center,
                 modifier = Modifier.fillMaxSize()
-            ) { CircularProgressIndicator(modifier = Modifier.width(64.dp)) }
+            ) { ContainedLoadingIndicator() }
             else -> content()
         }
     }
