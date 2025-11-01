8fe26177eabb22cda5b1df7b8e7409b7996320cc
GdPaul1234 <paul.godin1234@outlook.fr>
2025-11-01 23:05:20 +0100

    chore: use ModalWideNavigationRail in MediaNavigationRail

--- MoviePipelineSegmentsValidatorUi/composeApp/src/commonMain/kotlin/com/gdpaul1234/movie_pipeline_segments_validator_ui/medias/navigation/MediasNavigationComponents.kt
+++ MoviePipelineSegmentsValidatorUi/composeApp/src/commonMain/kotlin/com/gdpaul1234/movie_pipeline_segments_validator_ui/medias/navigation/MediasNavigationComponents.kt
@@ -6,16 +6,12 @@ package com.gdpaul1234.movie_pipeline_segments_validator_ui.medias.navigation
  */
 
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.*
 import androidx.compose.material3.adaptive.WindowAdaptiveInfo
 import androidx.compose.material3.adaptive.currentWindowAdaptiveInfo
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.window.core.layout.WindowSizeClass
@@ -46,103 +42,103 @@ fun MediasNavigationWrapper(
     val adaptiveInfo = currentWindowAdaptiveInfo()
     val navLayoutType = getNavLayoutType(adaptiveInfo)
 
-    val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
-    val coroutineScope = rememberCoroutineScope()
-
-    // Avoid opening the modal drawer when there is a bottom nav bar, but always allow closing an open drawer.
-    val gesturesEnabled = drawerState.isOpen || navLayoutType == MediasNavigationType.NAVIGATION_RAIL
-
-    ModalNavigationDrawer(
-        drawerState = drawerState,
-        gesturesEnabled = gesturesEnabled,
-        drawerContent = {
-            ModalNavigationDrawerContent(
-                currentMediaStateEq = currentMediaStateEq,
-                navigateToTopLevelDestination = navigateToTopLevelDestination,
-                onDrawerClicked = {  coroutineScope.launch { drawerState.close() } }
-            )
+    Scaffold(
+        bottomBar = {
+            if (navLayoutType == MediasNavigationType.NAVIGATION_BAR) {
+                MediaBottomNavigationBar(
+                    currentMediaStateEq = currentMediaStateEq,
+                    navigateToTopLevelDestination = navigateToTopLevelDestination,
+                )
+            }
         },
-    ) {
-        Scaffold(
-            bottomBar = {
-                if (navLayoutType == MediasNavigationType.NAVIGATION_BAR) {
-                    MediaBottomNavigationBar(
+        contentWindowInsets = WindowInsets.displayCutout
+    ) { paddingValues ->
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues)
+                .consumeWindowInsets(paddingValues)
+        ) {
+            Row {
+                if (navLayoutType == MediasNavigationType.NAVIGATION_RAIL) {
+                    MediaNavigationRail(
                         currentMediaStateEq = currentMediaStateEq,
                         navigateToTopLevelDestination = navigateToTopLevelDestination,
                     )
                 }
-            },
-            contentWindowInsets = WindowInsets.displayCutout
-        ) { paddingValues ->
-            Box(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(paddingValues)
-                    .consumeWindowInsets(paddingValues)
-            ) {
-                Row {
-                    if (navLayoutType == MediasNavigationType.NAVIGATION_RAIL) {
-                        MediaNavigationRail(
-                            currentMediaStateEq = currentMediaStateEq,
-                            navigateToTopLevelDestination = navigateToTopLevelDestination,
-                            onDrawerClicked = { coroutineScope.launch { drawerState.open() } },
-                        )
-                    }
 
-                    // Content area to show the NavigationRail in context
-                    Box(modifier = Modifier.fillMaxSize().weight(1f)) {
-                        content(navLayoutType)
-                    }
+                // Content area to show the NavigationRail in context
+                Box(modifier = Modifier.fillMaxSize().weight(1f)) {
+                    content(navLayoutType)
                 }
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3ExpressiveApi::class, ExperimentalMaterial3Api::class)
 @Composable
 fun MediaNavigationRail(
     currentMediaStateEq: Media.State,
     navigateToTopLevelDestination: (mediaStateEq: Media.State) -> Unit,
-    onDrawerClicked: () -> Unit = {},
 ) {
-    NavigationRail(
-        modifier = Modifier.fillMaxHeight(),
-        containerColor = MaterialTheme.colorScheme.inverseOnSurface,
+    val state = rememberWideNavigationRailState()
+    val scope = rememberCoroutineScope()
+    val headerDescription = when {
+        state.targetValue == WideNavigationRailValue.Expanded -> stringResource(Res.string.close_drawer)
+        else -> stringResource(Res.string.navigation_drawer)
+    }
+
+    ModalWideNavigationRail(
+        state = state,
+        colors = WideNavigationRailDefaults.colors(MaterialTheme.colorScheme.inverseOnSurface),
+        header = {
+            // Header icon button should have a tooltip.
+            TooltipBox(
+                positionProvider = TooltipDefaults.rememberTooltipPositionProvider(),
+                tooltip = { PlainTooltip { Text(headerDescription) } },
+                state = rememberTooltipState(),
+            ) {
+                IconButton(
+                    modifier = Modifier.padding(start = 24.dp),
+                    onClick = {
+                        scope.launch {
+                            if (state.targetValue == WideNavigationRailValue.Expanded) state.collapse()
+                            else state.expand()
+                        }
+                    },
+                ) {
+                    Icon(
+                        painter = painterResource(when {
+                            state.targetValue == WideNavigationRailValue.Expanded -> Res.drawable.menu_open_24px
+                            else -> Res.drawable.menu_24px
+                        }),
+                        contentDescription = headerDescription
+                    )
+                }
+            }
+        },
     ) {
-        Column(
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.spacedBy(4.dp),
-        ) {
-            NavigationRailItem(
-                selected = false,
-                onClick = onDrawerClicked,
+        TOP_LEVEL_DESTINATIONS.forEach { mediaStateDestination ->
+            WideNavigationRailItem(
+                railExpanded = state.targetValue == WideNavigationRailValue.Expanded,
                 icon = {
                     Icon(
-                        painter = painterResource(Res.drawable.menu_24px),
-                        contentDescription = stringResource(Res.string.navigation_drawer),
+                        painter = painterResource(mediaStateDestination.icon),
+                        contentDescription = stringResource(mediaStateDestination.iconTextResource),
                     )
                 },
+                label = {
+                    Text(
+                        modifier = Modifier.padding(horizontal = 4.dp),
+                        text = stringResource(mediaStateDestination.iconTextResource),
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis
+                    )
+                },
+                selected = currentMediaStateEq == mediaStateDestination.mediaStateEq,
+                onClick = { navigateToTopLevelDestination(mediaStateDestination.mediaStateEq) },
             )
-            Spacer(Modifier.height(8.dp)) // NavigationRailHeaderPadding
-            Spacer(Modifier.height(4.dp)) // NavigationRailVerticalPadding
-        }
-
-        LazyColumn(
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.spacedBy(4.dp),
-        ) {
-            items(items = TOP_LEVEL_DESTINATIONS, key = { it.mediaStateEq }) { mediaStateDestination ->
-                NavigationRailItem(
-                    selected = currentMediaStateEq == mediaStateDestination.mediaStateEq,
-                    onClick = { navigateToTopLevelDestination(mediaStateDestination.mediaStateEq) },
-                    icon = {
-                        Icon(
-                            painter = painterResource(mediaStateDestination.icon),
-                            contentDescription = stringResource(mediaStateDestination.iconTextResource),
-                        )
-                    },
-                )
-            }
         }
     }
 }
@@ -174,55 +170,3 @@ fun MediaBottomNavigationBar(
         }
     }
 }
-
-@Composable
-fun ModalNavigationDrawerContent(
-    currentMediaStateEq: Media.State,
-    navigateToTopLevelDestination: (mediaStateEq: Media.State) -> Unit,
-    onDrawerClicked: () -> Unit = {},
-) {
-    ModalDrawerSheet {
-        Column(modifier = Modifier.padding(16.dp)) {
-            Column(verticalArrangement = Arrangement.spacedBy(4.dp)) {
-                Row(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalArrangement = Arrangement.End,
-                    verticalAlignment = Alignment.CenterVertically,
-                    ) {
-                    IconButton(
-                        modifier = Modifier.size(64.dp),
-                        onClick = onDrawerClicked
-                    ) {
-                        Icon(
-                            painter = painterResource(Res.drawable.menu_open_24px),
-                            contentDescription = stringResource(Res.string.close_drawer),
-                        )
-                    }
-                }
-            }
-
-            LazyColumn(horizontalAlignment = Alignment.CenterHorizontally) {
-                items(TOP_LEVEL_DESTINATIONS, { it.mediaStateEq }) { mediaStateDestination ->
-                    NavigationDrawerItem(
-                        selected = currentMediaStateEq == mediaStateDestination.mediaStateEq,
-                        label = {
-                            Text(
-                                text = stringResource(mediaStateDestination.iconTextResource),
-                                modifier = Modifier.padding(horizontal = 16.dp),
-                            )
-                        },
-                        icon = {
-                            Icon(
-                                painter = painterResource(mediaStateDestination.icon),
-                                contentDescription = stringResource(mediaStateDestination.iconTextResource),
-                            )
-                        },
-                        colors = NavigationDrawerItemDefaults.colors(unselectedContainerColor = Color.Transparent),
-                        onClick = { navigateToTopLevelDestination(mediaStateDestination.mediaStateEq) },
-                    )
-                }
-            }
-        }
-    }
-}
-
