c1ca7a2b108aca34c5d6c774dd65b7c1543b5f16
GdPaul1234 <paul.godin1234@outlook.fr>
2025-11-16 18:18:52 +0100

    feat: set default size for desktop target

--- MoviePipelineSegmentsValidatorUi/composeApp/src/jvmMain/kotlin/com/gdpaul1234/movie_pipeline_segments_validator_ui/main.kt
+++ MoviePipelineSegmentsValidatorUi/composeApp/src/jvmMain/kotlin/com/gdpaul1234/movie_pipeline_segments_validator_ui/main.kt
@@ -1,12 +1,18 @@
 package com.gdpaul1234.movie_pipeline_segments_validator_ui
 
+import androidx.compose.ui.unit.DpSize
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import androidx.compose.ui.window.rememberWindowState
 import com.gdpaul1234.movie_pipeline_segments_validator_ui.core.database.createDataStore
 
 fun main() = application {
+    val state = rememberWindowState(size = DpSize(1366.dp, 768.dp))
+
     Window(
         onCloseRequest = ::exitApplication,
+        state = state,
         title = "MoviePipelineSegmentsValidatorUi",
     ) {
         App(dataStore = createDataStore())
