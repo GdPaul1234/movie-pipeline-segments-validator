0695ae4a0d49b2e534ba2fcbb7370ba63524baf8
GdPaul1234 <paul.godin1234@outlook.fr>
2025-11-16 18:48:05 +0100

    feat: handle back action in ListDetailPaneScaffold

--- MoviePipelineSegmentsValidatorUi/composeApp/build.gradle.kts
+++ MoviePipelineSegmentsValidatorUi/composeApp/build.gradle.kts
@@ -37,6 +37,7 @@ kotlin {
             implementation(compose.components.uiToolingPreview)
             implementation(libs.androidx.lifecycle.viewmodelCompose)
             implementation(libs.androidx.lifecycle.runtimeCompose)
+            implementation(libs.ui.backhandler)
 
             implementation(libs.androidx.adaptative)
             implementation(libs.androidx.adaptative.layout)
